3efd850a348072a1512cc036740d0c43a70ed047
Pave1P <145470076+Pave1P@users.noreply.github.com>
2026-01-31 16:14:13 +0200

    Delete eshop directory

--- eshop/backend/Migration/2026_01_29_install.sql
+++
@@ -1,5 +0,0 @@
-create table migration(
-    ID int not null auto_incriment,
-    NAME varchar(255) not null,
-    PRIMARY KEY (ID)
-);
--- eshop/public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-$application = 'main';
-require __DIR__ . '/../backend/kernel/web.php';
